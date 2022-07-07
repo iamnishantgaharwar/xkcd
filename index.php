23edaf0ce578ab65151c7845039e5854cb23ccfa
Nishant Gaharwar <iamnishantgaharwar@gmail.com>
2022-07-07 19:47:10 +0530

    Change-log: navbar created

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>XKCD Challenge</title>
+</head>
+<body>
+    <nav class="nav-container">
+        <div class="nav-logo">XKCD</div>
+        <div class="nav-menu-content">
+            <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
